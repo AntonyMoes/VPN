cf2d1e926ac5e7c60b3768cc60ef934f02ca05f6
Savely Ermolenko <savva.gorodetsky@gmail.com>
2018-05-02 14:31:57 +0300

    local

--- src/rwr.cpp
+++ src/rwr.cpp
@@ -2,13 +2,13 @@
 
 int cread(int fd, char *buf, int n){
 
- int nread;
+    int nread;
 
- if((nread=read(fd, buf, n)) < 0 && errno != EAGAIN){
-   //perror("Reading data");
-   //exit(1);
- }
- return nread;
+    if((nread=read(fd, buf, n)) < 0 && errno != EAGAIN){
+        //perror("Reading data");
+        //exit(1);
+    }
+    return nread;
 }
 
 /**************************************************************************
@@ -17,13 +17,13 @@ int cread(int fd, char *buf, int n){
 **************************************************************************/
 int cwrite(int fd, char *buf, int n){
 
- int nwrite;
+    int nwrite;
 
- if((nwrite=write(fd, buf, n)) < 0){
-   perror("Writing data");
-   exit(1);
- }
- return nwrite;
+    if((nwrite=write(fd, buf, n)) < 0){
+        perror("Writing data");
+        exit(1);
+    }
+    return nwrite;
 }
 
 /**************************************************************************
@@ -32,17 +32,17 @@ int cwrite(int fd, char *buf, int n){
 **************************************************************************/
 int read_n(int fd, char *buf, int n) {
 
- int nread, left = n;
+    int nread, left = n;
 
- while(left > 0) {
-   if ((nread = cread(fd, buf, left)) == 0){
-     return 0 ;
-   }else {
-     left -= nread;
-     buf += nread;
-   }
- }
- return n;
+    while(left > 0) {
+        if ((nread = cread(fd, buf, left)) == 0){
+            return 0 ;
+        }else {
+            left -= nread;
+            buf += nread;
+        }
+    }
+    return n;
 }
 
 /**************************************************************************
@@ -50,13 +50,13 @@ int read_n(int fd, char *buf, int n) {
 **************************************************************************/
 void do_debug(int debug, char *msg, ...){
 
- va_list argp;
+    va_list argp;
 
- if(debug) {
-   va_start(argp, msg);
-   vfprintf(stderr, msg, argp);
-   va_end(argp);
- }
+    if(debug) {
+        va_start(argp, msg);
+        vfprintf(stderr, msg, argp);
+        va_end(argp);
+    }
 }
 
 /**************************************************************************
@@ -64,23 +64,23 @@ void do_debug(int debug, char *msg, ...){
 **************************************************************************/
 void my_err(char *msg, ...) {
 
- va_list argp;
+    va_list argp;
 
- va_start(argp, msg);
- vfprintf(stderr, msg, argp);
- va_end(argp);
+    va_start(argp, msg);
+    vfprintf(stderr, msg, argp);
+    va_end(argp);
 }
 
 void usage(char *progname) {
-  fprintf(stderr, "Usage:\n");
-  fprintf(stderr, "%s -i <ifacename> [-s|-c <serverIP>] [-p <port>] [-u|-a] [-d]\n", progname);
-  fprintf(stderr, "%s -h\n", progname);
-  fprintf(stderr, "\n");
-  fprintf(stderr, "-i <ifacename>: Name of interface to use (mandatory)\n");
-  fprintf(stderr, "-s|-c <serverIP>: run in server mode (-s), or specify server address (-c <serverIP>) (mandatory)\n");
-  fprintf(stderr, "-p <port>: port to listen on (if run in server mode) or to connect to (in client mode), default 55555\n");
-  fprintf(stderr, "-u|-a: use TUN (-u, default) or TAP (-a)\n");
-  fprintf(stderr, "-d: outputs debug information while running\n");
-  fprintf(stderr, "-h: prints this help text\n");
-  exit(1);
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, "%s -i <ifacename> [-s|-c <serverIP>] [-p <port>] [-u|-a] [-d]\n", progname);
+    fprintf(stderr, "%s -h\n", progname);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "-i <ifacename>: Name of interface to use (mandatory)\n");
+    fprintf(stderr, "-s|-c <serverIP>: run in server mode (-s), or specify server address (-c <serverIP>) (mandatory)\n");
+    fprintf(stderr, "-p <port>: port to listen on (if run in server mode) or to connect to (in client mode), default 55555\n");
+    fprintf(stderr, "-u|-a: use TUN (-u, default) or TAP (-a)\n");
+    fprintf(stderr, "-d: outputs debug information while running\n");
+    fprintf(stderr, "-h: prints this help text\n");
+    exit(1);
 }
